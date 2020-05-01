bc0f955c088ad21af88f9c37c7b24b1e38a3f8a4
Trevor Sauerbrey <trevor.sauerbrey@roswellbiotech.com>
2020-04-30 21:05:07 -0700

    final

--- scrape_mars.py
+++ scrape_mars.py
@@ -5,54 +5,54 @@ import pandas as pd
 
 def scrape():
     # URLs of pages to be scraped
-    nasa_mars_news_url = 'https://mars.nasa.gov/news/'
-    mars_image_url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
-    mars_weather_url = 'https://twitter.com/marswxreport?lang=en'
-    mars_facts_url = 'http://space-facts.com/mars/'
-    mars_hemispheres_url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+    mars_nasa = 'https://mars.nasa.gov/news/'
+    mars_image = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
+    mars_weather = 'https://twitter.com/marswxreport?lang=en'
+    mars_facts = 'http://space-facts.com/mars/'
+    mars_hemispheres = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
 
-    news_response = requests.get(nasa_mars_news_url)
-    soup = BeautifulSoup(news_response.text, 'html.parser')
+    response_mars_nasa = requests.get(mars_nasa)
+    soup = BeautifulSoup(response_mars_nasa.text, 'html.parser')
     results = soup.find_all(class_="slide")
-    titles_list = []
-    paragraphs_list = []
+    list_titles = []
+    list_pp = []
 
     for result in results:
             links = result.find_all('a')
             title = links[1].text
             paragraph = result.find(class_="rollover_description_inner").text
-            titles_list.append(title)
-            paragraphs_list.append(paragraph)
-    news_title = titles_list[0]
-    news_p = paragraphs_list[0]
+            list_titles.append(title)
+            list_pp.append(paragraph)
+    news_title = list_titles[0]
+    news_p = list_pp[0]
 
 
-    image_response = requests.get(mars_image_url)
-    soup = BeautifulSoup(image_response.text, 'html.parser')
+    response_image = requests.get(mars_image)
+    soup = BeautifulSoup(response_image.text, 'html.parser')
     results = soup.find_all(class_="carousel_items")
 
     for result in results:
         article = result.find('article', class_="carousel_item")
-        article_link = article['style']
-        cleaned_article_link = article['style'].lstrip('background-image: url(')
-        cleaned_article_link = cleaned_article_link.rstrip(');')
-    cleaned_article_link = cleaned_article_link.replace("'", "")
-    featured_image_link = 'https://www.jpl.nasa.gov'+cleaned_article_link
+        link_article = article['style']
+        cleaned_link_article = article['style'].lstrip('background-image: url(')
+        cleaned_link_article = cleaned_link_article.rstrip(');')
+    cleaned_link_article = cleaned_link_article.replace("'", "")
+    feat_image_link = 'https://www.jpl.nasa.gov'+cleaned_link_article
 
 
-    weather_response = requests.get(mars_weather_url)
-    soup = BeautifulSoup(weather_response.text, 'html.parser')
+    response_weather = requests.get(mars_weather)
+    soup = BeautifulSoup(response_weather.text, 'html.parser')
     results = soup.find_all(class_="content")
-    tweets_list = []
+    list_tweets = []
     for result in results:
         tweet = result.find('p', class_="TweetTextSize").text
-        tweets_list.append(tweet)
-    mars_weather = tweets_list[0]
+        list_tweets.append(tweet)
+    mars_weather = list_tweets[0]
 
     executable_path = {'executable_path': 'chromedriver.exe'}
     browser = Browser('chrome', **executable_path, headless=False)
-    usgs_base_url= 'https://astrogeology.usgs.gov'
-    links_list = []
+    usgs_base = 'https://astrogeology.usgs.gov'
+    list_links = []
     hemispheres_response = requests.get(mars_hemispheres_url)
     soup = BeautifulSoup(hemispheres_response.text, 'html.parser')
 
@@ -60,31 +60,31 @@ def scrape():
     for result in results:
         links = result.find('a')
         link=links['href']
-        links_list.append(usgs_base_url+link)
-        browser.visit(mars_hemispheres_url)
+        list_links.append(usgs_base+link)
+        browser.visit(mars_hemispheres)
 
-    hemispheres_image_urls = []
-    titles_list = []
+    hemispheres_image = []
+    list_titles = []
 
     for x in range(0, 4):
-        browser.visit(links_list[x])
+        browser.visit(list_links[x])
         html = browser.html
         soup = BeautifulSoup(html, 'html.parser')
         images = soup.find(class_='downloads')
         image = images.find('a')
         image_url= image['href']
-        hemispheres_image_urls.append(image_url)
+        hemispheres_image.append(image_url)
         titles = soup.find('h2', class_='title')
         title=titles.text
         title=title.strip('Enhanced')
-        titles_list.append(title)
+        list_titles.append(title)
 
-    hemispheres_dict = {'Title': titles_list,
-                        'URL': hemispheres_image_urls }
-    scraped_dict = {'Title': titles_list,
-                    'URL': hemispheres_image_urls,
+    hemispheres_dict = {'Title': list_titles,
+                        'URL': hemispheres_image }
+    scraped_dict = {'Title': list_titles,
+                    'URL': hemispheres_image,
                     'Weather': mars_weather,
-                    'Featured Image': featured_image_link,
+                    'Featured Image': feat_image_link,
                     'News Title': news_title,
                     'News Body': news_p
                }

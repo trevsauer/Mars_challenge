a163c87a498a7ce45b3cf5b5007f0e627292b410
Trevor Sauerbrey <trevor.sauerbrey@roswellbiotech.com>
2020-04-30 21:02:05 -0700

    final push

--- app.py
+++ app.py
@@ -11,19 +11,17 @@ app = Flask(__name__)
 @app.route("/")
 def welcome():
     mars_db = db.mars_db.find()
-    """List all available api routes."""
+
     return (
         f"Available Routes:<br/>"
         f"/scrape"
     )
-
 
 @app.route("/scrape")
 def web_scraper():
     scrape_mars.scrape();
     db.mars_db.insert(scrape_mars.scrape())
     return jsonify(scrape_mars.scrape())
-
 
 if __name__ == '__main__':
     app.run(debug=True)
